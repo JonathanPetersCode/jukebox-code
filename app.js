fb150e3170fa220425fdef0b798ace54175bab0c
Jonathan <Jonathan.Peters@arvatosystems.com>
2022-02-11 11:18:45 -0500

    add axios GET request

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const { default: axios } = require("axios");
-const musicBrainz = "https://musicbrainz.org/ws/2/release/f6ab348d-81a5-4bdd-9fae-2536fba72c20?inc=recordings";
+const musicBrainzSampleRelease = "http://musicbrainz.org/ws/2/release/59211ea4-ffd2-4ad9-9a4e-941d3148024a?inc=artist-credits+labels+discids+recordings";
 
-const getJsonResponseFromAPI = axios.get(musicBrainz).then(function (response) {
-    console.log(response);
+axios.get(musicBrainzSampleRelease).then(res => {
+    console.log(`statusCode: ${res.status}`)
+    console.log(res)
+  })
+  .catch(error => {
+    console.error(error)
   })
