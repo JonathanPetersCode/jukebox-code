cf4e3968ce5036b614c9f62f05da07711f8dc600
Jonathan <Jonathan.Peters@arvatosystems.com>
2022-02-11 16:34:36 -0500

    remove musicbrainz, add deezermusic API

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const { default: axios } = require("axios");
-const musicBrainzSampleRelease = "http://musicbrainz.org/ws/2/release/59211ea4-ffd2-4ad9-9a4e-941d3148024a?inc=artist-credits+labels+discids+recordings";
 
-axios.get(musicBrainzSampleRelease).then(res => {
-    console.log(`statusCode: ${res.status}`)
-    console.log(res)
-  })
-  .catch(error => {
-    console.error(error)
+const kansasAlbum = "https://api.deezer.com/album/78167/"
+const kansasAlbum = "https://api.deezer.com/album/78167/"
+
+
+axios.get(deezerMusicSample).then(res => {
+    // console.log(`statusCode: ${res.status}`)
+    // console.log(Object.keys(res.data))
+    console.log(res.data)
   })
